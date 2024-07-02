40e64d7b757d7c501006df61fa46bb40acd1fede
sokolip <sokolip8@gmail.com>
2024-07-02 09:51:51 -0500

    Homework_6 done

---
+++ homework/ivan_sokolskii/Homework_6/Homework_6_task_1.py
@@ -0,0 +1,27 @@
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at," \
+       " dignissim vitae libero"
+words = []
+actual_word = ''
+for char in text:
+    if char.isalpha():
+        actual_word += char
+    else:
+        if actual_word:
+            words.append(actual_word)
+            actual_word = ''
+        if char == ',' or char == '.':
+            words.append(char)
+print(words)
+add = 'ing'
+result_word = []
+for word in words:
+    if word == ',' or word == '.':
+        result_word.append(word)
+    else:
+        result_word.append(word + add)
+print(result_word)
+final_text = (' '.join(result_word))
+print(final_text)
+clear_text = final_text.replace(" ,", ",")
+clear_text_1 = clear_text.replace(" .", ".")
+print(clear_text_1)
