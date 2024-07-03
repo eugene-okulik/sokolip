31851d9ab665a0d340773d7d9bf91331aa3586f1
sokolip <sokolip8@gmail.com>
2024-07-03 13:39:42 -0500

    add blank lines  around function

--- homework/ivan_sokolskii/Homework_7/task_3.py
+++ homework/ivan_sokolskii/Homework_7/task_3.py
@@ -1,11 +1,15 @@
 result_1 = 'результат операции: 42'
 result_2 = 'результат операции: 514'
 result_3 = 'результат работы программы: 9'
+
+
 def print_num(result):
     num_index = result.index(':') + 2
     num = int(result[num_index:])
     num += 10
     print(num)
+
+
 print_num(result_1)
 print_num(result_2)
 print_num(result_3)
