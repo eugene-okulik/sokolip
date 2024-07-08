8d76679753d6611cc2185171eb3e89b7473aa1ea
sokolip <sokolip8@gmail.com>
2024-07-08 13:52:14 -0500

    add min/max/avg temperature

--- homework/ivan_sokolskii/Homework_9/task_1.py
+++ homework/ivan_sokolskii/Homework_9/task_1.py
@@ -5,4 +5,7 @@ print(python_date)
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29,
                 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
 new_temperatures = filter(lambda x: x > 28, temperatures)
-print(list(new_temperatures))
+new_temperatures = list(new_temperatures)
+print(max(new_temperatures))
+print(min(new_temperatures))
+print(((max(new_temperatures) + min(new_temperatures)) / 2))
