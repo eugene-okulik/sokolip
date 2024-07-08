cf2168d7949a919a24e6878e4af66f7319b9b0c5
sokolip <sokolip8@gmail.com>
2024-07-08 13:42:27 -0500

    delete space

--- homework/ivan_sokolskii/Homework_9/task_1.py
+++ homework/ivan_sokolskii/Homework_9/task_1.py
@@ -1,5 +1,5 @@
 import datetime
-my_date  = "Jan 15, 2023 - 12:05:33"
+my_date = "Jan 15, 2023 - 12:05:33"
 python_date = datetime.datetime.strptime(my_date, '%b %d, %Y - %H:%M:%S')
 print(python_date)
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29,
