806eecd03d1a3a1be6edcb59a937cf69fabfb776
sokolip <sokolip8@gmail.com>
2024-07-16 10:06:59 -0500

    task_1 done

--- homework/ivan_sokolskii/Homework_12/task_1.py
+++ homework/ivan_sokolskii/Homework_12/task_1.py
@@ -7,27 +7,75 @@ class Flowers:
       self.price = price
       self.length = length
 
+
 class Rose(Flowers):
     def __init__(self, name, color, fresher, live_time, price, length, prickle):
         super().__init__(name, color, fresher, live_time, price, length)
         self.prickle = prickle
+
+
 class Daisy(Flowers):
     def __init__(self, name, color, fresher, live_time, price, length, bush):
         super().__init__(name, color, fresher, live_time, price, length)
         self.bush = bush
+
 
 class Tulpan(Flowers):
     def __init__(self, name, color, fresher, live_time, price, length):
         super().__init__(name, color, fresher, live_time, price, length)
 
 
-white_rose = Rose('w_rose', 'white', 2, 10, 100, 17, True)
-red_rose = Rose('r_rose', 'red', 1, 11, 120, 17, False)
-blue_rose  = Rose('b_rose', 'red', 2, 8, 105, 18, True)
-big_daisy = Daisy('b_daisy' 'white', 3, 6, 80, 15, False)
-small_daisy = Daisy('s_daisy', 'pink', 4, 5, 97, 18, True)
-red_tulpan = Tulpan('r_tulpan', 'red', 4, 7, 120, 20)
-white_tulpan = Tulpan('w_tulpan', 'white', 5, 12, 105, 21)
-pink_tulpan = Tulpan('p_tulpan', 'pink', 3, 14, 150, 21)
+class Buket():
+    def __init__(self):
+        self.flowers = []
 
-class Buket()
+
+    def add_flowers(self, flower):
+        self.flowers.append(flower)
+
+
+    def cost(self):
+        total_cost = sum(flower.price for flower in self.flowers)
+        print(f'Стоимость букета: {total_cost}')
+
+
+    def live_time_buket(self):
+        live_time = sum(flower.live_time for flower in self.flowers)/len(self.flowers)
+        print(f'Время жизни букета: {live_time}')
+
+
+    def sort_flowers_by(self, key='color'):
+        if key == 'color':
+            self.flowers.sort(key=lambda x: x.color)
+            for flower in buket_1.flowers:
+                print(flower)
+        elif key == 'name':
+            self.flowers.sort(key=lambda x: x.name)
+            for flower in buket_1.flowers:
+                print(flower)
+        elif key == 'length':
+            self.flowers.sort(key=lambda x: x.length)
+            for flower in buket_1.flowers:
+                print(flower)
+        elif key == 'price':
+            self.flowers.sort(key=lambda x: x.price)
+            for flower in buket_1.flowers:
+                print(flower)
+
+
+white_rose = Rose('w_rose', 'white', 2, 10, 100, 17, 'yes')
+red_rose = Rose('r_rose', 'red', 1, 11, 120, 17, 'no')
+blue_rose  = Rose('b_rose', 'red', 2, 8, 105, 18, 'yes')
+big_daisy = Daisy('b_daisy' 'white', 3, 6, 80, 15, 'no', True)
+small_daisy = Daisy('s_daisy', 'pink', 4, 5, 97, 18, 'yes')
+red_tulp = Tulpan('r_tulp', 'red', 4, 7, 120, 20)
+white_tulp = Tulpan('w_tulp', 'white', 5, 12, 105, 21)
+pink_tulp = Tulpan('p_tulp', 'pink', 3, 14, 150, 21)
+buket_1 = Buket()
+buket_1.add_flowers(white_rose)
+buket_1.add_flowers(red_rose)
+buket_1.add_flowers(big_daisy)
+buket_1.add_flowers(pink_tulp)
+buket_1.cost()
+buket_1.live_time_buket()
+buket_1.sort_flowers_by('price')
