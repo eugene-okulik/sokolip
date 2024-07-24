2472573b60a8cd3845f908e42ba36101a64b19d5
sokolip <sokolip8@gmail.com>
2024-07-24 15:05:25 -0500

    all requered changes done

--- homework/ivan_sokolskii/Homework_15/task_1.py
+++ homework/ivan_sokolskii/Homework_15/task_1.py
@@ -7,6 +7,13 @@ db = mysql.connect(
     database='st-onl'
 )
 cursor = db.cursor(dictionary=True)
+create_student = '''
+INSERT INTO students(name, second_name)
+VALUES (%s, %s)
+'''
+cursor.execute(create_student, ('Matte', 'Manatee'))
+last_student_id = cursor.lastrowid
+print('идентификатор студента: ', last_student_id)
 create_groups = '''
 INSERT INTO `groups` (title, start_date, end_date) 
 VALUES (%s, %s, %s)
@@ -14,13 +21,12 @@ VALUES (%s, %s, %s)
 cursor.execute(create_groups, ('QAgroup353', 'may 2024', 'oct 2024'))
 last_id_group = cursor.lastrowid
 print('идентификатор группы: ', last_id_group)
-create_student = '''
-INSERT INTO students(name, second_name, group_id)
-VALUES (%s, %s, %s)
+add_student_in_group = f'''
+UPDATE students
+SET group_id = {last_id_group} 
+WHERE id = {last_student_id}
 '''
-cursor.execute(create_student, ('Matte', 'Manatee', last_id_group))
-last_student_id = cursor.lastrowid
-print('идентификатор студента: ', last_student_id)
+cursor.execute(add_student_in_group)
 create_books_query = '''
 INSERT INTO books (title, taken_by_student_id)
 VALUES (%s, %s);
@@ -39,40 +45,29 @@ bratiay_karamazovy_id = bratiay_karamazovy_id_value['id']
 python_for_everyone_id_query = cursor.execute("SELECT id from books ORDER BY id DESC LIMIT 1 OFFSET 2")
 python_for_everyone_id_value = cursor.fetchone()
 python_for_everyone_id = python_for_everyone_id_value['id']
-print('идентификатор книги_1: ', curse_of_yoga_id)
-print('идентификатор книги_2: ', bratiay_karamazovy_id)
-print('идентификатор книги_3: ', python_for_everyone_id)
+print('ID book_1: ', curse_of_yoga_id)
+print('ID book_2: ', bratiay_karamazovy_id)
+print('ID book_3: ', python_for_everyone_id)
 create_subjets_query = "INSERT INTO subjets(title) VALUES(%s)"
-cursor.executemany(
-    create_subjets_query, [
-        ('Fizra',),
-        ('OBJ',)
-    ]
-)
-last_subjets_id = cursor.lastrowid
-penultimate_subjets_id = cursor.execute("SELECT id from subjets ORDER BY id DESC LIMIT 1 OFFSET 1")
-penultimate_subjets_id_value = cursor.fetchone()
-penultimate_subjets_id = penultimate_subjets_id_value['id']
-print('идентификатор физры', penultimate_subjets_id)
+cursor.execute(create_subjets_query, ('Fizra',))
+fizra_subjets_id = cursor.lastrowid
+print('ID fizra', fizra_subjets_id)
+cursor.execute(create_subjets_query, ('OBJ',))
+obj_subjet_id = cursor.lastrowid
+print('ID OBJ: ', obj_subjet_id)
 create_lessons_query = "INSERT INTO lessons(title, subject_id) VALUES (%s, %s)"
-cursor.executemany(
-    create_lessons_query, [
-        ('first_less_bio', last_subjets_id),
-        ('second_less_math', penultimate_subjets_id),
-        ('trith_less_wor', last_subjets_id),
-        ('five_less_fis', penultimate_subjets_id)
-    ]
-)
+cursor.execute(create_lessons_query,('first_less_bio', obj_subjet_id))
+first_less_fis_id = cursor.lastrowid
+print('ID first lesson: ', first_less_fis_id)
+cursor.execute(create_lessons_query, ('second_less_math', fizra_subjets_id))
+second_less_math_id = cursor.lastrowid
+print('ID second lesson: ', second_less_math_id)
+cursor.execute(create_lessons_query, ('trith_less_wor', obj_subjet_id))
+trith_less_wor_id = cursor.lastrowid
+print('ID Trith lesson: ', trith_less_wor_id)
+cursor.execute(create_lessons_query, ('five_less_fis', fizra_subjets_id))
 five_less_fis_id = cursor.lastrowid
-trith_less_wor_id_query = cursor.execute("SELECT id from lessons ORDER BY id DESC LIMIT 1 OFFSET 1")
-trith_less_wor_id_value = cursor.fetchone()
-trith_less_wor_id = trith_less_wor_id_value['id']
-second_less_math_id_query = cursor.execute("SELECT id from lessons ORDER BY id DESC LIMIT 1 OFFSET 2")
-second_less_math_id_value = cursor.fetchone()
-second_less_math_id = second_less_math_id_value['id']
-first_less_bio_id_query = cursor.execute("SELECT id from lessons ORDER BY id DESC LIMIT 1 OFFSET 3")
-first_less_bio_id_value = cursor.fetchone()
-first_less_bio_id = first_less_bio_id_value['id']
+print('ID five lesson: ', five_less_fis_id)
 marks_lesson_by_id = f"SELECT lesson_id FROM marks WHERE student_id = {last_student_id}"
 create_marks_query = '''
 INSERT INTO marks (value, lesson_id, student_id)
@@ -83,7 +78,7 @@ cursor.executemany(
         ('5', five_less_fis_id, last_student_id),
         ('3', trith_less_wor_id, last_student_id),
         ('2', second_less_math_id, last_student_id),
-        ('4', first_less_bio_id, last_student_id)
+        ('4', first_less_fis_id, last_student_id)
     ]
 )
 db.commit()
@@ -110,5 +105,6 @@ JOIN subjets on lessons.subject_id = subjets.id
 WHERE students.id = {last_student_id}
 '''
 all_info_by_student_id = cursor.execute(all_info_by_student_id_query)
-print(cursor.fetchall())
+all_information = cursor.fetchone ()
+print('All infomation about student: ', all_information)
 db.close()
