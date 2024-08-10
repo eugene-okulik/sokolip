982876160bbe78690ec24d1f2af3cd785fc2242e
sokolip <sokolip8@gmail.com>
2024-08-10 17:08:22 -0500

    linter

--- homework/ivan_sokolskii/Homework_19/test_pytest.py
+++ homework/ivan_sokolskii/Homework_19/test_pytest.py
@@ -40,36 +40,36 @@ def test_one_post():
 
 
 @pytest.mark.parametrize("body", (
-                {
-                    "name": "Apple MacBook Pro 16",
-                    "data": {
-                        "year": 2019,
-                        "price": 1849.99,
-                        "CPU model": "Intel Core i9",
-                        "Hard disk size": "1 TB"
-                    }
-                },
-                {
-                    "name": "Apple MacBook Pro 17",
-                    "data": {
-                        "year": 2019,
-                        "price": 1849.99,
-                        "CPU model": "Intel Core i9",
-                        "Hard disk size": "1 TB"
-                    }
-                },
-                {
-                    "name": "Apple MacBook Pro 19",
-                    "data": {
-                        "year": 2019,
-                        "price": 1849.99,
-                        "CPU model": "Intel Core i9",
-                        "Hard disk size": "1 TB"
-                    }
-                }
+    {
+        "name": "Apple MacBook Pro 16",
+        "data": {
+            "year": 2019,
+            "price": 1849.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB"
+        }
+    },
+    {
+        "name": "Apple MacBook Pro 17",
+        "data": {
+            "year": 2019,
+            "price": 1849.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB"
+        }
+    },
+    {
+        "name": "Apple MacBook Pro 19",
+        "data": {
+            "year": 2019,
+            "price": 1849.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB"
+        }
+    }
 
-                                )
-                        )
+)
+)
 def test_add_object(body):
     print('before test')
     url = 'https://api.restful-api.dev/objects'
