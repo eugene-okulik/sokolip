dc15ac9ab94307f1210d830590b731945be57bba
sokolip <sokolip8@gmail.com>
2024-09-25 14:21:17 -0500

    Homework_done

--- homework/ivan_sokolskii/Homework_26/task_1.py
+++ homework/ivan_sokolskii/Homework_26/task_1.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page, Dialog, expect
+from playwright.sync_api import Page, Dialog, expect, BrowserContext
 
 
 def test_click_alert(page: Page):
@@ -13,6 +13,20 @@ def test_click_alert(page: Page):
     after_click_text = page.locator("//p[@id='result-text']")
     expect(after_click_text).to_have_text('Ok')
 
-def test_check_tab(page: Page):
+def test_check_tab(page: Page, context: BrowserContext):
     page.goto('https://www.qa-practice.com/elements/new_tab/button')
+    link = page.locator('#new-page-button')
+    with context.expect_page() as new_page_event:
+        link.click()
 
+    new_page = new_page_event.value
+    result = new_page.locator('#result-text')
+    expect(result).to_have_text('I am a new page in a new tab')
+    check_button = page.get_by_role('link', name='Click')
+    expect(check_button).to_be_enabled()
+
+def test_change_button_color(page: Page):
+    page.goto('https://demoqa.com/dynamic-properties')
+    button_locator = page.get_by_role('button', name='Color Change')
+    expect(button_locator).to_have_css('color', 'rgb(220, 53, 69)')
+    button_locator.click()
